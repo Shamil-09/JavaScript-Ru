452573df24ef0d9ea3e646f6778754d558977b53
Shamil <Liga009@mail.ru>
2020-06-18 10:21:55 -0500

    finished lesson 12

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-const answer = prompt('Are you 18?', '');
-console.log(typeof (answer));
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+  b = prompt('Насколько оцените его?', ''),
+  c = prompt('Один из последних просмотренных фильмов?', ''),
+  d = prompt('Насколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
